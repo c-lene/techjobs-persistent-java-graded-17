38f62242bdc33ae766fcce07cb466e2774058974
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-11-20 17:40:40 -0600

    CT completed - Task 2 - Part 1 & 2 complete

--- queries.sql
+++ queries.sql
@@ -1,6 +1,17 @@
 --Part 1
 
+--Columns:
+--    id: int PK
+--    employer: varchar(255)
+--    name: varchar(255)
+--    skills: varchar(255)
+
+
 --Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
 
 --Part 3
 
